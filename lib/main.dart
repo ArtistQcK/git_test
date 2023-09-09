3e4aeeecd16ba0389a3d7881a37e93a78f579aea
ArtistQcK <ArtistQcK@gmail.com>
2023-09-09 13:14:28 +0300

    Experimental commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     print('object');
     print('object');
     print('object');
+    print('Experimental');
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
