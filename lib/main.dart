5be4473eacaf3b3954a19dbbe747627473bc231b
ArtistQcK <ArtistQcK@gmail.com>
2023-09-11 11:44:01 +0300

    friend commit  2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print('Hello i am your friend');
-    print('object');
+    print(' print(Hello i am your friend);');
     print('object');
     print('Experimental');
     print('Youtube');
